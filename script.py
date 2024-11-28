6e3a7510e0a790ea05da2720fabf020de7884f10
Ammar <you@example.com>
2024-11-28 19:06:17 +0530

    stable

--- script.py
+++ script.py
@@ -3,9 +3,10 @@ import sqlite3
 
 
 data = {
-    'Username': 'yourUsername',
-    'Password': 'yourPassword',
-    'DBTag': 'firstdatabase'
+    'Username':"u2",
+    'Password':'p2',
+    'DBPassword': 'dbp2',
+    'DBTag': 'dbt2'
 }
 
 response = requests.post('http://localhost:8080/register', json=data)
@@ -15,6 +16,7 @@ print(response.json())
 conn = sqlite3.connect("models/UserDB.db")
 cur = conn.cursor()
 cur.execute("select * from Users")
+conn.commit()
 row = cur.fetchall()
 for r in row:
     print(r)
